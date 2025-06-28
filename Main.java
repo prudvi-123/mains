2949abbfa931280af8c5f03041ebf5dcb578e1cf
prudvi-123 <126657551+prudvi-123@users.noreply.github.com>
2025-06-28 12:34:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello DevOps"); } }
+public class Main { public static void main(String[] args) { System.out.println("Hello DevOps this is prudvi"); } }
